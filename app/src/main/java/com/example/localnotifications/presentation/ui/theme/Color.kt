686854006588a331b3ea5b51a0366b641414bb00
kenjr <kenedy@myhechar.pro>
2023-01-06 16:08:22 +0300

    moved the files to a diff. dir

--- app/src/main/java/com/example/localnotifications/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/localnotifications/presentation/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.localnotifications.ui.theme
+package com.example.localnotifications.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/example/localnotifications/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/localnotifications/presentation/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.localnotifications.ui.theme
+package com.example.localnotifications.presentation.ui.theme
 
 import android.app.Activity
 import android.os.Build
