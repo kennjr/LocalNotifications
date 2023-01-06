af4750bd6fcd3323c581e735e2d7f8c1f9582c98
kenjr <kenedy@myhechar.pro>
2023-01-06 16:16:16 +0300

    replaced the mainscreen with the setupnavgraph fun, after introducing the navigation

--- app/src/main/java/com/example/localnotifications/MainActivity.kt
+++ app/src/main/java/com/example/localnotifications/MainActivity.kt
@@ -3,8 +3,7 @@ package com.example.localnotifications
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,8 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.localnotifications.ui.theme.LocalNotificationsTheme
+import androidx.navigation.compose.rememberNavController
+import com.example.localnotifications.presentation.main.MainScreen
+import com.example.localnotifications.presentation.navigation.SetupNavGraph
+import com.example.localnotifications.presentation.ui.theme.LocalNotificationsTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,12 +30,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-                        Button(onClick = {
-                            notificationService.showNotification(Counter.value)
-                        }) {
-                            Text(text = "Show Notification")
-                        }
+                    Column(modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center){
+//                        Box(modifier = Modifier
+//                            .fillMaxHeight(.1f)
+//                            .fillMaxWidth(),
+//                            contentAlignment = Alignment.Center){
+//                            Button(onClick = {
+//                                notificationService.showNotification(Counter.value)
+//                            }) {
+//                                Text(text = "Show Notification")
+//                            }
+//                        }
+                        SetupNavGraph(navHostController = rememberNavController())
                     }
                 }
             }
