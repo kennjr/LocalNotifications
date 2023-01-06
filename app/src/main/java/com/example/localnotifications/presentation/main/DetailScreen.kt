32fc3189afaec198c3665a1f9461386ceed0a4b0
kenjr <kenedy@myhechar.pro>
2023-01-06 16:09:02 +0300

    created a detail screen for finding out whether the nav options work

---
+++ app/src/main/java/com/example/localnotifications/presentation/main/DetailScreen.kt
@@ -0,0 +1,31 @@
+package com.example.localnotifications.presentation.main
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun DetailScreen(argument: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = argument,
+            style = TextStyle(
+                fontStyle = FontStyle.Normal,
+                fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                fontWeight = FontWeight.Bold
+            )
+        )
+    }
+}
